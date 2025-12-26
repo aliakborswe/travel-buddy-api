f6781e04be3a57410e21f26366997f82a3830cf1
aliakborswe <aliakbor.js@gmail.com>
2025-12-26 19:33:18 +0600

    update server

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import { Server } from "http";
 import mongoose from "mongoose";
 import app from "./app";
 import { envVars } from "./app/config/env";
+import { TravelPlanService } from "./app/modules/travel-plans/travel.service";
+import nodeCron from "node-cron";
 
 let server: Server;
 
@@ -10,6 +12,21 @@ const startServer = async () => {
     await mongoose.connect(envVars.DB_URL);
 
     console.log("Connected to DB!!");
+
+    // Run initial status update on server start
+    await TravelPlanService.updateTravelStatuses();
+
+    // Schedule daily status updates at midnight (00:00)
+    nodeCron.schedule("0 0 * * *", async () => {
+      console.log("Running scheduled travel status update...");
+      await TravelPlanService.updateTravelStatuses();
+    });
+
+    // Also run status updates every 6 hours for more frequent checks
+    nodeCron.schedule("0 */6 * * *", async () => {
+      console.log("Running periodic travel status update...");
+      await TravelPlanService.updateTravelStatuses();
+    });
 
     server = app.listen(envVars.PORT, () => {
       console.log(`Server is listening to port ${envVars.PORT}`);
